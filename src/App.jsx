06e1bdc1fc5f08f1246fbfb89b9895c14bb2b726
Vaibhav Parashar <parasharv592@gmail.com>
2024-04-29 17:39:32 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom' 
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <Home/>
+
+
+
+    </>
+  )
+}
+
+export default App
